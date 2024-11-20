b04a49b517d886a6ddd28c57f719030ecd842d8e
AustinYou <coverme.carrymenow@gmail.com>
2024-11-20 21:31:31 +0800

    加入聊天對話框

---
+++ js/tawk.to.js
@@ -0,0 +1,13 @@
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/673dde104304e3196ae5abfc/1id4qms6b';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
